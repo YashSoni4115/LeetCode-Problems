c104029e4fa5c2f7c0de3866828c54cd373ff5ab
YashSoni4115 <yashsonig@gmail.com>
2025-10-08 20:36:53 -0400

    Add 0001 Two Sum, easy, python

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # LeetCode Problems
+
+## Solutions
+
+- [0001 Two Sum](easy/0001_two_sum.py) , easy, python
---
+++ easy/0001_two_sum.py
@@ -0,0 +1,21 @@
+"""
+------------------------------------------------------------
+LeetCode 1, Two Sum, difficulty easy, language python
+Saved at 2025-10-08 20:36:52
+------------------------------------------------------------
+"""
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        num_to_index = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in num_to_index:
+                return [num_to_index[complement], i]
+            num_to_index[num] = i
+end
